bd716fa7f7ab11763c8a58b0182702e56fe54e17
Istiak Ahamed <sifatistiak@gmail.com>
2019-08-10 04:04:40 +0600

    Update Sign_in.dart
    Error in FirebaseUser user.  Casting error

--- firebase_showcase/lib/pages/setup/sign_in.dart
+++ firebase_showcase/lib/pages/setup/sign_in.dart
@@ -57,7 +57,7 @@ class _LoginPageState extends State<LoginPage> {
     if(_formKey.currentState.validate()){
       _formKey.currentState.save();
       try{
-        FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+        FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password)).user;
         Navigator.push(context, MaterialPageRoute(builder: (context) => Home(user: user)));
       }catch(e){
         print(e.message);
